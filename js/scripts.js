92d1943a03d8c89c97932b57b15a96ce17ba5ebf
create initial version <>
2018-06-07 20:12:16 +0200

    Establish the font-size for all devices as 16px; enlarged <p> font-size for devices <767px; added text to project2; created scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+console.log("Hello guys! I would like say you that")
+var foo = {baz1: "Frontend Development", baz2: "UI Design", goo: "important"}, baz3 ="UX Design";
+console.log("%s has %d %s aspects. I describe it as %s, %s and %s.", "Web design ", 3, foo.goo, baz3, foo.baz2, foo.baz1);
