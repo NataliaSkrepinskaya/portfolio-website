10e8b417d01dcef0e88b48d620887b0ebd3aab22
create initial version <>
2018-06-19 15:55:52 +0200

    Add js for scrolling, add new img for projects pages, changed some img for project pages, add new content for project pages, add img for home page.

--- js/scripts.js
+++ js/scripts.js
@@ -27,12 +27,39 @@ $("#validate").bind("click", validate); //clicking on button "submit"//
 
 
 
-$(document).ready(function(){ //smooth scolling//
-	$("nav").on("click","a", function (event) {
-		event.preventDefault(); //deleted standart clicking
-		var id  = $(this).attr('href'), //get the identificator from href
-			top = $(id).offset().top; //calculating the height of the section
-
-		$('body,html').animate({scrollTop: top}, 3000); //animated scrolling  to top duiring 3000 мс
-	});
-});
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
